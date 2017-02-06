2e87354dbca390024f58f94a9010411a0b3b87f9
sasakazu <dropout.college2014@gmail.com>
2017-02-06 19:09:54 +0900

    config.serve_static_assets = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+# ここから編集
+
+config.serve_static_assets = true
+
+
+
+
+
+
+
+
+
+
 end
