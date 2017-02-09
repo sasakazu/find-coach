5ee2da52518af40f350da6e838c56f7eef2bf347
sasakazu <dropout.college2014@gmail.com>
2017-02-09 12:52:02 +0900

    presence true done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   mount_uploader :icon, ImageUploader
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+
 
 
 
