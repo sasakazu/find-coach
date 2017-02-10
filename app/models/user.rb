101bccf51c07f400c1d9035998389ce4c0e76c09
sasakazu <dropout.college2014@gmail.com>
2017-02-10 13:18:03 +0900

    session login done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,18 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :password_digest, presence: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
 
 
+
+
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   root 'pages#home'
 
   resources :users
   resources :coachs
 
   get 'pages/about' => 'pages#about'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+
+
+
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
