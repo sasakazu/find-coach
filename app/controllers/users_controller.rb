45c853ccdb7b6f98c7140ed848d1339d8a47dee6
sasakazu <dropout.college2014@gmail.com>
2017-02-20 19:11:13 +0900

    action mailer start lern done...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      SampleMailer.send_when_update(current_user).deliver
+
       flash[:success] = "Profile updated"
           redirect_to @user
     else
